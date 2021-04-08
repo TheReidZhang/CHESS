17b3ae4a24ecca48df31898137decd87a72918ec
Zijian Zhang <zzhang38@terpmail.umd.edu>
2021-04-07 23:50:22 -0400

    chess_ui implemented

---
+++ chess_ui/src/board/InGameMenu.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Dropdown } from 'react-bootstrap';
+import { useHistory } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function InGameMenu() {
+
+    const history = useHistory();
+
+    return (
+        
+        <Dropdown>
+            <Dropdown.Toggle variant="secondary" id="InGameMenu" size="sm">
+                MENU
+            </Dropdown.Toggle>
+            <Dropdown.Menu style={{minWidth: "auto"}}>
+                <Dropdown.Item onClick={() => history.push("/")} > 
+                    <div style={{textAlign: "center", fontSize: "50%", fontWeight: "bold"}}>
+                        QUIT
+                    </div>
+                </Dropdown.Item>
+            </Dropdown.Menu>
+        </Dropdown>
+        
+    );
+}
+
+export default InGameMenu;
---
+++ chess_ui/src/board/WithMoveValidation.js
@@ -0,0 +1,252 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import Chessboard from "chessboardjsx";
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Container from 'react-bootstrap/Container'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './ChessBoard.css';
+import Info from './Info';
+import InGameMenu from "./InGameMenu";
+import PromotionMenu from "./PromotionMenu";
+
+
+class ChessBoard extends Component {
+  static propTypes = { children: PropTypes.func };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      session_id: parseInt(props.session_id),
+      fen: "start",
+      // custom square styles
+      squareStyles: {},
+      // square with the currently clicked piece
+      pieceSquare: "",
+      // currently clicked square
+      square: "",
+      // array of past game moves
+      history: [],
+      validMoves: [],
+      turn: "Loading...",
+      status: "Loading...",
+      role: "Queen"
+    };
+  }
+
+  async componentDidMount() {
+    const fen_response = await fetch('/chess/info', {
+      method: 'POST',
+      body: JSON.stringify({
+      session_id: this.state.session_id})
+    });
+    
+    if (!fen_response.ok) {
+      throw Error("None-existed session.");
+    }
+    
+    const fen_json = await fen_response.json();
+    const fen = fen_json["fen"];
+    const status = fen_json["status"];
+    const turn = fen_json["turn"]
+    const history = fen_json["history"];
+    const promotion = fen_json["promotion"];
+    this.setState({fen: fen, status: status, turn: turn, history: history, promotion: promotion});
+    this.setState(({ validMoves, history }) => ({
+      squareStyles: squareStyling({ validMoves, history })
+    }));
+  }
+
+  // show possible moves
+  highlightSquare = (sourceSquare, squaresToHighlight) => {
+    const highlightStyles = [sourceSquare, ...squaresToHighlight].reduce(
+      (a, c) => {
+        return {
+          ...a,
+          ...{
+            [c]: {
+              background:
+              "radial-gradient(circle, #0d8230 36%, transparent 40%)",
+              borderRadius: "50%"
+            }
+          },
+          ...squareStyling({
+            validMoves: this.state.validMoves,
+            history: this.state.history
+          })
+        };
+      },
+      {}
+    );
+
+    this.setState(({ squareStyles }) => ({
+      squareStyles: { ...squareStyles, ...highlightStyles }
+    }));
+  };
+
+  setPromotion = (role) => {
+    this.setState({role: role})
+  }
+
+
+  onSquareClick = async (square) => {
+    // Clicked some piece
+    if (this.state.pieceSquare !== "") {
+      const update_response = await fetch('/chess/update', {
+        method: 'POST',
+        body: JSON.stringify({ 
+          src: this.state.pieceSquare,
+          tar: square,
+          session_id: this.state.session_id,
+          role: this.state.role
+         })
+      });
+      const update_json = await update_response.json();
+      const update_valid = update_json["valid"];
+
+      if (update_valid) {   
+        const fen_response = await fetch('/chess/info', {
+          method: 'POST',
+          body: JSON.stringify({
+          session_id: this.state.session_id})
+        });
+        const fen_json = await fen_response.json();
+        const fen = fen_json["fen"];
+        const status = fen_json["status"];
+        const turn = fen_json["turn"];
+        const history = fen_json["history"];
+        const promotion = fen_json["promotion"];
+        this.setState({
+          fen: fen,
+          pieceSquare: "",
+          status: status,
+          turn: turn,
+          history: history,
+          validMoves: [],
+          promotion: promotion
+        });
+        
+
+        this.setState(({ validMoves, history }) => ({
+          squareStyles: squareStyling({ validMoves, history })
+        }));
+
+        if (promotion) {
+          
+        }
+        
+        const update_is_being_checked = update_json["is_being_checked"];
+        if (update_is_being_checked) {
+          setTimeout(function() {
+            alert('check!');
+           }, 0);
+        }
+        const update_game_status = update_json["game_status"];
+        if (update_game_status !== "Continue") {
+          setTimeout(function() {
+            alert(update_game_status);
+           }, 0);
+        }
+        
+         
+        return;
+      }
+    
+    }
+    
+    this.setState(({ validMoves, history }) => ({
+      squareStyles: squareStyling({ validMoves, history })
+    }));
+    const response = await fetch('/chess/' + this.state.session_id + '/'+square);
+    const json = await response.json();
+    const moves = json["moves"];
+    const squaresToHighlight = [];
+    for (var i = 0; i < moves.length; i++) {
+      squaresToHighlight.push(moves[i]);
+    }
+    this.setState({pieceSquare: square, validMoves: squaresToHighlight});
+    this.highlightSquare(square, squaresToHighlight);
+  };
+
+  render() {
+    const { fen, squareStyles, turn, status, promotion } = this.state;
+    
+    return this.props.children({
+      turn,
+      status,
+      promotion,
+      squareStyles,
+      position: fen,
+      onSquareClick: this.onSquareClick,
+      setPromotion: this.setPromotion
+    });
+  }
+}
+
+export default function WithMoveValidation(props) {
+
+  return (
+    <div>
+      <ChessBoard session_id={props.match.params.session_id}>
+        {({        
+          turn,
+          status,
+          promotion,
+          position, 
+          squareStyles,
+          onSquareClick,
+          setPromotion
+        }) => ([ 
+          <Container fluid style={{width:"100vw"}}>
+            <Row>
+                <Info turn={turn} status={status}/>
+                <Col style={{textAlign: "right"}}>
+                  <InGameMenu/>
+                </Col>
+            </Row>
+          </Container>,
+          <div className="board-center">
+            <Row> 
+            <Chessboard
+              id="ChessBoard"
+              width={350}
+              position={position}
+              boardStyle={{
+                borderRadius: "5px",
+                boxShadow: `0 5px 15px rgba(0, 0, 0, 0.5)`
+              }}
+              squareStyles={squareStyles}
+              onSquareClick={onSquareClick}
+              draggable={false}
+            />
+            <PromotionMenu show={promotion} setPromotion={setPromotion}/>
+            </Row>
+          </div>] 
+        )}
+      </ChessBoard>
+    </div>
+  );
+}
+
+const squareStyling = ({ validMoves, history }) => {
+  const sourceSquare = history.length && history[history.length - 1].src;
+  var srcStyle = "rgba(255, 255, 0, 0.4)"
+  if (validMoves.includes(sourceSquare)) {srcStyle = "radial-gradient(circle, #0d8230 36%, transparent 40%), rgba(255, 255, 0, 0.4)"} 
+  
+  const targetSquare = history.length && history[history.length - 1].tar;
+  var tarStyle = "rgba(255, 255, 0, 0.4)"
+  if (validMoves.includes(targetSquare)) {tarStyle = "radial-gradient(circle, #0d8230 36%, transparent 40%), rgba(240, 255, 0, 0.4)"} 
+
+  return {
+    ...(history.length && {
+      [sourceSquare]: {
+        background: srcStyle
+      }
+    }),
+    ...(history.length && {
+      [targetSquare]: {
+        background: tarStyle
+      }
+    })
+  };
+};
