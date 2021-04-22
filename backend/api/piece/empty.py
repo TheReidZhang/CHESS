9c74f58f8fc2f8196a4d288cf7328fee0952b63e
Xiao Yang <xyang777@terpmail.umd.edu>
2021-04-22 17:48:53 -0400

    add comments

--- backend/api/piece/empty.py
+++ backend/api/piece/empty.py
@@ -3,7 +3,15 @@ from api.piece.piece_interface import PieceInterface
 
 class Empty(PieceInterface):
     def get_moves(self) -> list:
+        """
+        This is empty piece meaning at this square there is no pieces and no moves.
+        :return: empty list meaning no moves
+        """
         return []
 
     def to_string(self) -> str:
+        """
+        For debugging use, simply print *
+        :return: a string "*"
+        """
         return "*"
